07b8a1a107c24e767c92b4eba842d8791f3d3a48
kevin downs <pyrosrppltoo@gmail.com>
2017-06-10 15:51:32 -0700

    concourse

--- Linux/Ubuntu/16.04/floppy_files/auto-start-concourse.sh
+++ Linux/Ubuntu/16.04/floppy_files/auto-start-concourse.sh
@@ -2,4 +2,5 @@
 ip route get 8.8.8.8 | awk '{print $NF; exit}'
 external_ip=$?
 export CONCOURSE_EXTERNAL_URL=http://$external_ip:8080
-sudo docker-compose /home/vagrant/docker-compose.yml up
+sudo docker-compose -f /home/vagrant/docker-compose.yml up
+
